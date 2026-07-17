77eeb108c9759dd71d39f123f15537af63cdd55c
itsdanjc <dan@itsdanjc.com>
2026-07-17 18:21:06 +0100

    Added placeholder variables.

---
+++ js/config.js
@@ -0,0 +1,18 @@
+/*
+    Copyright (c) 2026 itsdanjc.
+    Licensed under MIT.
+*/
+
+
+export const placeholders = {
+    track: {
+        line_1: "Placeholder Title",
+        line_2: "Placeholder Content",
+        line_3: "Placeholder Footer"
+    },
+    programme: {
+        line_1: "On Air",
+        line_2: "24/7 Hits",
+        line_3: "example.com/radio"
+    }
+}
