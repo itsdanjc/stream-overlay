f4eaf7f7743a40519db3e154ac1c955c848db326
itsdanjc <dan@itsdanjc.com>
2026-07-20 15:52:02 +0100

    Added build script

---
+++ scripts/banner.js
@@ -0,0 +1,8 @@
+export const BANNER = `/*
+    Stream Overlay
+    
+    https://github.com/itsdanjc/stream-overlay
+
+    Copyright (c) 2026 itsdanjc.
+    Licensed under MIT.
+*/`;
---
+++ scripts/build.js
@@ -0,0 +1,29 @@
+import * as esbuild from 'esbuild'
+import { mkdir, readFile, writeFile } from "node:fs/promises"
+import htmlPlugin from "@chialab/esbuild-plugin-html";
+
+import { BANNER } from './banner.js';
+
+const OUT_DIR = "./dist";
+const TO_BUILD = ["**.html"];
+
+// Define and make output directory
+await mkdir(OUT_DIR, { recursive: true });
+
+// Build using html files as entrypoints.
+let html = await esbuild.build({
+    entryPoints: TO_BUILD,
+    outdir: OUT_DIR,
+    assetNames: 'static/[name]',
+    chunkNames: '[name].min',
+    bundle: true,
+    banner: {
+        js: BANNER,
+        css: BANNER
+    },
+    loader: {
+        ".ttf": "copy",
+    },
+    minify: true,
+    plugins: [htmlPlugin()],
+});
